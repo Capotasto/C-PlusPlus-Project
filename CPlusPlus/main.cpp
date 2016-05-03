e5e0beada0f0a1f23b546bd6ee7c037b56778bc1
Capotasto <rocker519804@gmail.com>
2016-05-02 23:34:34 -0700

    Initial Commit

---
+++ CPlusPlus/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  CPlusPlus
+//
+//  Created by Norio Egi on 5/2/16.
+//  Copyright © 2016 Capotasto. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
